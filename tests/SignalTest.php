33f3bcc765873d472c6f149d3c765b665e978835
otsch <otsch@users.noreply.github.com>
2022-11-15 23:06:36 +0100

    Add tests for Signal

---
+++ tests/SignalTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use Otsch\Ppq\Config;
+use Otsch\Ppq\Signal;
+
+beforeEach(function () {
+    Config::setPath(__DIR__ . '/_testdata/config/ppq.php');
+
+    if (!file_exists(__DIR__ . '/_testdata/datapath/signal')) {
+        touch(__DIR__ . '/_testdata/datapath/signal');
+    }
+
+    file_put_contents(__DIR__ . '/_testdata/datapath/signal', '');
+});
+
+it('sets the stop signal', function () {
+    $signal = new Signal();
+
+    expect($signal->isStop())->toBeFalse();
+
+    $signal->setStop();
+
+    expect($signal->isStop())->toBeTrue();
+});
+
+it('resets the stop signal', function () {
+    $signal = new Signal();
+
+    $signal->setStop();
+
+    expect($signal->isStop())->toBeTrue();
+
+    $signal->reset();
+
+    expect($signal->isStop())->toBeFalse();
+});
